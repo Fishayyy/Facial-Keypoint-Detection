17352a490a15b554bcdc1b6b18b5f0392b699773
Fishayyy <Mr.ATFisher@gmail.com>
2021-03-03 10:32:09 -0800

    data_analysis.py prints out scatter plots for the x,y coordinates of all 15 features

--- data_analysis.py
+++ data_analysis.py
@@ -1,3 +1,16 @@
+# To re-download the data use command: kaggle competitions download -c facial-keypoints-detection
+# Must have kaggle api installed: https://www.kaggle.com/docs/api
+
 import numpy as np 
 import pandas as pd 
+import matplotlib.pyplot as plt
 
+with open('training.csv') as csvfile:    
+    data = pd.read_csv(csvfile, delimiter = ',')
+
+labels = data.columns
+
+for i in range(0,len(labels)-1,2):
+    data.plot.scatter(x=labels[i], y=labels[i+1])
+
+plt.show()
