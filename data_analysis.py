770f7c24e323c4b3bf6e9da361ac8dd48e92079e
Austin Fisher <Mr.ATFisher@gmail.com>
2021-04-28 11:54:36 -0700

    Made progress

--- data_analysis.py
+++ data_analysis.py
@@ -1,3 +1,13 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+import cv2
+from tqdm.notebook import tqdm
 
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import Sequential, Model, load_model
+from keras.layers import Activation, Convolution2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout, Conv2D,MaxPool2D, ZeroPadding2D
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
+from keras.optimizers import Adam
